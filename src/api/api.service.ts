660d7fa9c20f4382ba77f10eaa760c3fa4ec6190
renato-av <renato.avela.17@gmail.com>
2023-01-13 11:21:40 -0500

    <fix>: api url fixed

--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -2,7 +2,7 @@ import { useAuthStore } from '@/config/store/auth'
 import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios'
 import { StatusCodes } from 'http-status-codes'
 
-const API_BASE_URL: string = import.meta.env.VITE_API_BASE_URL ?? ''
+const API_BASE_URL: string = import.meta.env.VITE_API_BASE_URL ?? '/api/v1'
 
 interface ApiConfig {
   url: string
